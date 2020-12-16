b60d727d65b78d881e38158e1b85fc50a1191eb8
Tom Donohue <monodot@gmail.com>
2020-12-16 20:34:16 +0000

    Update to Fuse 7.8

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # simple-camel-app
+
+To run:
+
+    mvn clean spring-boot:run
+    
+This will start a REST service on port 8080 using the embedded servlet container, Undertow:
+
+    $ curl http://localhost:8080/camel/hello
+    Hello world!
+
+Note that the default servlet path for Camel is `/camel`.
--- src/main/java/xyz/tomd/MySpringBootRouter.java
+++ src/main/java/xyz/tomd/MySpringBootRouter.java
@@ -3,17 +3,12 @@ package xyz.tomd;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
-/**
- * A simple Camel route that triggers from a timer and calls a bean and prints to system out.
- * <p/>
- * Use <tt>@Component</tt> to make Camel auto detect this route when starting.
- */
 @Component
 public class MySpringBootRouter extends RouteBuilder {
 
     @Override
     public void configure() {
-        from("jetty:http://0.0.0.0:8080/hello")
+        from("servlet:/hello")
                 .setBody(constant("Hello world!"));
     }
 
